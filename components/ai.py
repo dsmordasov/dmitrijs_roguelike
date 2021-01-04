1046631e590186b1566091d437e9ad22d92b28ae
Dmitrij <d.s.mordasov@gmail.com>
2021-01-04 17:35:30 +0100

    ranged scrolls working! (p9 done)

--- components/ai.py
+++ components/ai.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
-from typing import List, Tuple, TYPE_CHECKING
+import random
+from typing import List, Optional, Tuple, TYPE_CHECKING
 
 import numpy as np # type: ignore
 import tcod
 
-from actions import Action, MeleeAction, MovementAction, WaitAction
+from actions import Action, BumpAction, MeleeAction, MovementAction, WaitAction
 
 if TYPE_CHECKING:
     from entity import Actor
@@ -67,3 +68,45 @@ class HostileEnemy(BaseAI):
             ).perform()
 
         return WaitAction(self.entity).perform()
+
+class ConfusedEnemy(BaseAI):
+    """
+    A confused enemy will stumble around aimlessly for a given number of turns.
+    If an actor occupies a tile it is randomly moving into, it will be attacked.
+    """
+    def __init__(
+        self, entity: Actor, previous_ai: Optional[BaseAI], turns_remaining: int
+    ):
+        super().__init__(entity)
+
+        self.previous_ai = previous_ai
+        self.turns_remaining = turns_remaining
+
+    def perform(self) -> None:
+        
+        if self.turns_remaining <= 0:
+            # Revert the AI back to the original state if the effect has run its course
+            self.engine.message_log.add_message(
+                f"The {self.entity.name} is no longer confused."
+            )
+            self.entity.ai = self.previous_ai
+        else:
+            # Pick a random direction
+            direction_x, direction_y = random.choice(
+                [
+                    (-1, -1), # NW
+                    (0, -1), # N
+                    (1, -1), # NE
+                    (-1, 0), # W
+                    (1, 0), # E
+                    (-1, 1), # SW
+                    (0, 1), # S
+                    (1, 1), # SQ
+                ]
+            )
+
+            self.turns_remaining -= 1
+
+            # The actor will either try to move or attack in the chosen direction
+            # In case the actor bumps into a wall, a turn is just wasted
+            return BumpAction(self.entity, direction_x, direction_y).perform()
--- components/consumable.py
+++ components/consumable.py
@@ -3,10 +3,12 @@ from __future__ import annotations
 from typing import Optional, TYPE_CHECKING
 
 import actions
+import components.ai
 import color
 import components.inventory
 from components.base_component import BaseComponent
 from exceptions import Impossible
+from input_handlers import AreaRangedAttackHandler, SingleRangedAttackHandler
 
 if TYPE_CHECKING:
     from entity import Actor, items
@@ -75,3 +77,73 @@ class LightningDamageConsumable(Consumable):
             self.consume()
         else:
             raise Impossible("No enemy is close enough to strike.")
+
+class ConfusionConsumable(Consumable):
+
+    def __init__(self, number_of_turns: int):
+        self.number_of_turns = number_of_turns
+
+    def get_action(self, consumer: Actor) -> Optional[actions.Action]:
+        self.engine.message_log.add_message(
+            "Select a target location.", color.needs_target
+        )
+        self.engine.event_handler = SingleRangedAttackHandler(
+            self.engine,
+            callback = (lambda xy: actions.ItemAction(consumer, self.parent, xy)),
+        )
+        return None
+    
+    def activate(self, action: action.ItemAction) -> None:
+        consumer = action.entity
+        target = action.target_actor
+
+        if not self.engine.game_map.visible[action.target_xy]:
+            raise Impossible("You cannot target an area that you cannot see")
+        if not target:
+            raise Impossible("You must select an enemy to target.")
+        if target is consumer:
+            raise Impossible("You cannot confuse yourself!")
+
+        self.engine.message_log.add_message(
+            f"The {target.name} is confused!",
+            color.status_effect_applied
+        )
+        target.ai = components.ai.ConfusedEnemy(
+            entity=target, previous_ai = target.ai, turns_remaining=self.number_of_turns
+        )
+        self.consume
+
+class FireballDamageConsumable(Consumable):
+    def __init__(self, damage: int, radius: int):
+        self.damage = damage
+        self.radius = radius
+
+    def get_action(self, consumer: Actor) -> Optional[actions.Action]:
+        self.engine.message_log.add_message(
+            "Select a target location.", color.needs_target
+        )
+        self.engine.event_handler = AreaRangedAttackHandler(
+            self.engine,
+            radius=self.radius,
+            callback=(lambda xy: actions.ItemAction(consumer, self.parent, xy))
+        )
+        return None
+    
+    def activate(self, action: actions.ItemAction) -> None:
+        target_xy = action.target_xy
+
+        if not self.engine.game_map.visible[target_xy]:
+            raise Impossible("You cannot target an area you cannot see")
+
+        targets_hit = False
+        for actor in self.engine.game_map.actors:
+            if actor.distance(*target_xy) <= self.radius:
+                self.engine.message_log.add_message(
+                    f"The {actor.name} is scorched by the fireball for {self.damage} damage!"
+                )
+                actor.fighter.take_damage(self.damage)
+                targets_hit = True
+        
+        if not targets_hit:
+            raise Impossible("There are no targets in the area of effect of the spell.")
+        self.consume()
